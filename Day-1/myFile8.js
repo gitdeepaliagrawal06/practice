27700a970245f2c8ee73147d0e55353d964786b1
Deepali Agrawal <gitdeepaliagrawal06@gmail.com>
2025-09-09 11:37:24 +0530

    hi

---
+++ Day-1/myFile8.js
@@ -0,0 +1,11 @@
+function onLoginBtnClick() {
+    console.log("LogIn Button Clicked");
+}
+
+function onUserNameBtnClick() {
+    console.log("UserName Button Clicked")
+}
+
+function onPasswordBtnClick() {
+    console.log("Password Button Clicked")
+}
